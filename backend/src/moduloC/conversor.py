8a9d084e89d24edc7b2735d33918d1f79679ecf1
Pjotah <artist.pjotah@gmail.com>
2025-09-19 00:01:57 -0300

    Recomeçando ;-;

---
+++ backend/src/moduloC/conversor.py
@@ -0,0 +1,15 @@
+import ctypes
+import os
+
+# Puxa a dll
+
+funcao = ctypes.CDLL("C:/Users/USER/Documents/GitHub/PIM_SISTEMA_ACADEMICO/Backend/src/moduloC/ra_gerador.dll")
+
+
+# Oq a funçao retorna?
+
+funcao.gerar_ra.restype = ctypes.c_int #retorna int
+
+# Função que será chamada externamente
+def geradorzin():
+    return funcao.gerar_ra()
